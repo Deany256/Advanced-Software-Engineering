cbf67e7c3de05e1233eb24d236043c4b69c161ee
Deany256 <gdeandude@gmail.com>
2023-11-13 00:03:55 +0000

    Created shape class to be inherited from for any added shapes

---
+++ Component1/Shape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+
+namespace Component1
+{
+    abstract class Shape
+    {
+        protected Color colour; // Shape's colour
+        protected int x, y;    // Position
+        protected bool isFilled; // Indicates whether the shape should be filled
+
+        public Shape(Color colour, int x, int y, bool isFilled)
+        {
+            this.colour = colour;
+            this.x = x;
+            this.y = y;
+            this.isFilled = isFilled;
+        }
+
+        public abstract void Draw(Graphics g);
+    }
+}
