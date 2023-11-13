c37cd63bd03650ecc11861fadf07a37df470d1d8
Deany256 <gdeandude@gmail.com>
2023-11-13 04:02:31 +0000

    Started making XML docs

--- Component1/Shape.cs
+++ Component1/Shape.cs
@@ -3,12 +3,38 @@ using System.Drawing;
 
 namespace Component1
 {
-    abstract class Shape
+    /// <summary>
+    /// Represents an abstract shape with a specified color, position, and fill status.
+    /// </summary>
+    public abstract class Shape
     {
-        protected Color colour; // Shape's colour
-        protected int x, y;    // Position
-        protected bool isFilled; // Indicates whether the shape should be filled
+        /// <summary>
+        /// Gets or sets the color of the shape.
+        /// </summary>
+        protected Color colour;
 
+        /// <summary>
+        /// Gets or sets the X-coordinate of the shape's position.
+        /// </summary>
+        protected int x;
+
+        /// <summary>
+        /// Gets or sets the Y-coordinate of the shape's position.
+        /// </summary>
+        protected int y;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the shape should be filled.
+        /// </summary>
+        protected bool isFilled;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Shape"/> class with the specified parameters.
+        /// </summary>
+        /// <param name="colour">The color of the shape.</param>
+        /// <param name="x">The X-coordinate of the shape's position.</param>
+        /// <param name="y">The Y-coordinate of the shape's position.</param>
+        /// <param name="isFilled">A value indicating whether the shape should be filled.</param>
         public Shape(Color colour, int x, int y, bool isFilled)
         {
             this.colour = colour;
@@ -17,6 +43,10 @@ namespace Component1
             this.isFilled = isFilled;
         }
 
+        /// <summary>
+        /// Draws the shape on the specified graphics surface.
+        /// </summary>
+        /// <param name="g">The graphics surface on which to draw the shape.</param>
         public abstract void Draw(Graphics g);
     }
 }
