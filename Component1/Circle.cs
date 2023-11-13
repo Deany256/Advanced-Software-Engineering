0e66fcd81a8a6f3951895d8e3bdc91b0219cc1c2
Deany256 <gdeandude@gmail.com>
2023-11-13 00:05:28 +0000

    Created Circle class that inherits from the shape class, was based off rectangle

---
+++ Component1/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+namespace Component1
+{
+    class Circle : Shape
+    {
+        private int radius;
+
+        public Circle(Color colour, int x, int y, int radius, bool isFilled)
+            : base(colour, x, y, isFilled)
+        {
+            this.radius = radius;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            using (Brush brush = new SolidBrush(colour))
+            {
+                if (isFilled)
+                {
+                    g.FillEllipse(brush, x - radius, y - radius, 2 * radius, 2 * radius);
+                }
+                else
+                {
+                    g.DrawEllipse(new Pen(brush), x - radius, y - radius, 2 * radius, 2 * radius);
+                }
+            }
+        }
+    }
+}
