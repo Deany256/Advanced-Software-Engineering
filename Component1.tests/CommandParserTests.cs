53959b613385b6b9402cb71a1a914865d979ce29
Deany256 <gdeandude@gmail.com>
2023-11-13 06:24:31 +0000

    Created some tests that the program passes on.

---
+++ Component1.tests/CommandParserTests.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Component1.tests
+{
+    [TestClass]
+    public class CommandParserTests
+    {
+        [TestMethod]
+        public void CheckSyntax_ValidCommand_NoExceptionThrown()
+        {
+            // Arrange
+            var form = new Form1();
+            var commandParser = new CommandParser(form);
+            string[] validCommand = { "moveto", "10", "20" };
+
+            // Act and Assert
+            Assert.IsFalse(ThrowsException<ArgumentException>(() => commandParser.CheckSyntax(validCommand)));
+        }
+
+        [TestMethod]
+        public void CheckSyntax_InvalidCommand_ThrowsException()
+        {
+            // Arrange
+            var form = new Form1();
+            var commandParser = new CommandParser(form);
+            string[] invalidCommand = { "invalidcommand" };
+
+            // Act and Assert
+            Assert.IsTrue(ThrowsException<ArgumentException>(() => commandParser.CheckSyntax(invalidCommand)));
+        }
+
+        [TestMethod]
+        public void CheckSyntax_ValidMovetoCommand_NoExceptionThrown()
+        {
+            // Arrange
+            var form = new Form1();
+            var commandParser = new CommandParser(form);
+            string[] validCommand = { "moveto", "10", "20" };
+
+            // Act and Assert
+            Assert.IsFalse(ThrowsException<ArgumentException>(() => commandParser.CheckSyntax(validCommand)));
+        }
+
+        [TestMethod]
+        public void CheckSyntax_InvalidMovetoCommand_ThrowsException()
+        {
+            // Arrange
+            var form = new Form1();
+            var commandParser = new CommandParser(form);
+            string[] invalidCommand = { "moveto", "10", "20", "30" }; // Extra parameter
+
+            // Act and Assert
+            Assert.IsTrue(ThrowsException<ArgumentException>(() => commandParser.CheckSyntax(invalidCommand)));
+        }
+
+        [TestMethod]
+        public void CheckSyntax_ValidDrawCircleCommand_NoExceptionThrown()
+        {
+            // Arrange
+            var form = new Form1();
+            var commandParser = new CommandParser(form);
+            string[] validCommand = { "drawcircle", "15", "true" }; // Valid drawcircle command
+
+            // Act and Assert
+            Assert.IsFalse(ThrowsException<ArgumentException>(() => commandParser.CheckSyntax(validCommand)));
+        }
+
+        [TestMethod]
+        public void CheckSyntax_InvalidDrawCircleCommand_ThrowsException()
+        {
+            // Arrange
+            var form = new Form1();
+            var commandParser = new CommandParser(form);
+            string[] invalidCommand = { "drawcircle", "invalidRadius", "true" }; // Invalid radius parameter
+
+            // Act and Assert
+            Assert.IsTrue(ThrowsException<ArgumentException>(() => commandParser.CheckSyntax(invalidCommand)));
+        }
+
+
+
+
+
+
+        [TestMethod]
+        public void ExecuteCommand_ValidCommand_NoExceptionThrown()
+        {
+            // Arrange
+            var form = new Form1();
+            var commandParser = new CommandParser(form);
+            string validCommand = "moveto 10 20";
+
+            // Act and Assert
+            Assert.IsFalse(ThrowsException<ArgumentException>(() => commandParser.ExecuteCommand(validCommand)));
+        }
+
+        [TestMethod]
+        public void ExecuteCommand_InvalidCommand_SendsErrorMessage()
+        {
+            // Arrange
+            var form = new Form1();
+            var commandParser = new CommandParser(form);
+            string invalidCommand = "invalidcommand";
+
+            // Act
+            commandParser.ExecuteCommand(invalidCommand);
+
+            // Assert
+            Assert.AreEqual("Error: Invalid command.", form.label1.Text);
+        }
+
+        [TestMethod]
+        public void ExecuteCommand_ValidMovetoCommand_UpdatesCoordinates()
+        {
+            // Arrange
+            var form = new Form1();
+            var commandParser = new CommandParser(form);
+            string validCommand = "moveto 10 20";
+
+            // Act
+            commandParser.ExecuteCommand(validCommand);
+
+            // Assert
+            Assert.AreEqual(10, commandParser.GetCurrentX());
+            Assert.AreEqual(20, commandParser.GetCurrentY());
+        }
+
+        [TestMethod]
+        public void ExecuteCommand_ValidSetColourCommand_SetsColor()
+        {
+            // Arrange
+            var form = new Form1();
+            var commandParser = new CommandParser(form);
+            string validCommand = "setcolour red";
+
+            // Act
+            commandParser.ExecuteCommand(validCommand);
+
+            // Assert
+            Assert.AreEqual(Color.Red, commandParser.currentColor);
+        }
+
+        [TestMethod]
+        public void ExecuteCommand_ValidDrawCircleCommand_DoesNotThrowException()
+        {
+            // Arrange
+            var form = new Form1();
+            var commandParser = new CommandParser(form);
+            string validCommand = "drawcircle 15 true"; // Valid drawcircle command
+
+            // Act and Assert
+            Assert.IsFalse(ThrowsException<ArgumentException>(() => commandParser.ExecuteCommand(validCommand)));
+        }
+
+        [TestMethod]
+        public void ExecuteCommand_ValidDrawRectangleCommand_DrawsRectangle()
+        {
+            // Arrange
+            var form = new Form1();
+            var commandParser = new CommandParser(form);
+            string validCommand = "drawrectangle 20 30 false"; // Valid drawrectangle command
+
+            // Act
+            commandParser.ExecuteCommand(validCommand);
+
+            // Assert
+            // Add assertions based on the expected outcome of the command
+        }
+
+
+        private bool ThrowsException<T>(Action action) where T : Exception
+        {
+            try
+            {
+                action();
+                return false;
+            }
+            catch (T)
+            {
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Component1.tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Component1.tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Component1.tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("efdc969e-c4c9-4977-beac-4c4ecff18788")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
