bad0c5e86ffa2ffd2719713c9070b265ac91d6b1
Deany256 <gdeandude@gmail.com>
2023-11-13 00:04:39 +0000

    Created rectangle object that inherits from shape class

---
+++ Component1/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+
+namespace Component1
+{
+    class Rectangle : Shape
+    {
+        private int width, height;
+
+        public Rectangle(Color colour, int x, int y, int width, int height, bool isFilled)
+            : base(colour, x, y, isFilled)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            using (Brush brush = new SolidBrush(colour))
+            {
+                if (isFilled)
+                {
+                    g.FillRectangle(brush, x, y, width, height);
+                }
+                else
+                {
+                    g.DrawRectangle(new Pen(brush), x, y, width, height);
+                }
+            }
+        }
+    }
+}
