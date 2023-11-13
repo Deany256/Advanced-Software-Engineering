c26f08fd1811fc2ea8197fe7bed6c3c96d03f28a
Deany256 <gdeandude@gmail.com>
2023-11-13 04:05:01 +0000

    Added XML documentation for Rectangle

--- Component1/Rectangle.cs
+++ Component1/Rectangle.cs
@@ -2,10 +2,23 @@
 
 namespace Component1
 {
-    class Rectangle : Shape
+    /// <summary>
+    /// Represents a rectangle shape with a specified color, position, dimensions, and fill status.
+    /// </summary>
+    public class Rectangle : Shape
     {
-        private int width, height;
+        private int width;  // Width of the rectangle
+        private int height; // Height of the rectangle
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Rectangle"/> class with the specified parameters.
+        /// </summary>
+        /// <param name="colour">The color of the rectangle.</param>
+        /// <param name="x">The X-coordinate of the rectangle's position.</param>
+        /// <param name="y">The Y-coordinate of the rectangle's position.</param>
+        /// <param name="width">The width of the rectangle.</param>
+        /// <param name="height">The height of the rectangle.</param>
+        /// <param name="isFilled">A value indicating whether the rectangle should be filled.</param>
         public Rectangle(Color colour, int x, int y, int width, int height, bool isFilled)
             : base(colour, x, y, isFilled)
         {
@@ -13,6 +26,10 @@ namespace Component1
             this.height = height;
         }
 
+        /// <summary>
+        /// Draws the rectangle on the specified graphics surface.
+        /// </summary>
+        /// <param name="g">The graphics surface on which to draw the rectangle.</param>
         public override void Draw(Graphics g)
         {
             using (Brush brush = new SolidBrush(colour))
