787f3e87854bcbb8965a445639f27b396119f873
Deany256 <gdeandude@gmail.com>
2023-11-13 03:26:42 +0000

    established methods and created some basaic methods getters, a reset method and clear picturebox canvas

---
+++ Component1/CommandParser.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Component1
+{
+    class CommandParser
+    {
+        private Form1 formInstance;
+        private int currentX;
+        private int currentY;
+        private Color currentColor; // Store the current color
+
+        public CommandParser(Form1 formInstance)
+        {
+
+        }
+
+        public void Reset()
+        {
+            // set current coordinates to the top left of the picturebox
+            currentX = 0;
+            currentY = 0;
+        }
+
+        public void ClearPictureBox()
+        {
+            formInstance.pictureBox1.Image = null;
+
+        }
+
+        public void CheckSyntax(string[] commandArray)
+        {
+           
+        }
+
+        public void ExecuteCommand(string command)
+        {
+           
+        }
+
+        public int GetCurrentX()
+        {
+            return currentX;
+        }
+
+        public int GetCurrentY()
+        {
+            return currentY;
+        }
+
+        public void SendMessage(string message)
+        {
+            // Display the message in the label with the name 'label1'
+            formInstance.label1.Text = message;
+        }
+
+        private void SetColorByPreset(string presetColor)
+        {
+            
+        }
+
+        private void SetColorByRGB(int r, int g, int b)
+        {
+            
+        }
+        // Add methods for saving and loading a program to a text file
+
+        // Add methods for syntax checking (checking for valid commands and parameters)
+
+        // ... rest of the class remains the same ...
+
+        public static void FormatCode() { }
+
+        public void OpenFile(string save) 
+        {
+            
+        }
+
+        public void SaveFile(string savename, string[] commands) 
+        {
+            
+        }
+    }
+}
