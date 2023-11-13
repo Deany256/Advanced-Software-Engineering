122131420df0c81664a22f19f32c82b3045d76e7
Deany256 <gdeandude@gmail.com>
2023-11-13 03:11:50 +0000

    Reset Form1 and started some methods

--- Component1/Form1.Designer.cs
+++ Component1/Form1.Designer.cs
@@ -32,31 +32,33 @@
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(65, 704);
+            this.textBox1.Location = new System.Drawing.Point(40, 758);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(1058, 26);
+            this.textBox1.Size = new System.Drawing.Size(668, 26);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1168, 692);
+            this.button1.Location = new System.Drawing.Point(40, 690);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 50);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Submit";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(640, 58);
+            this.pictureBox1.Location = new System.Drawing.Point(640, 33);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(628, 617);
+            this.pictureBox1.Size = new System.Drawing.Size(628, 642);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
@@ -68,17 +70,39 @@
             this.textBox2.Size = new System.Drawing.Size(572, 641);
             this.textBox2.TabIndex = 3;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(174, 696);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 38);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Syntax";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(333, 700);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1327, 767);
+            this.ClientSize = new System.Drawing.Size(1327, 828);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -89,8 +113,10 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.Label label1;
     }
 }
 
--- Component1/Form1.cs
+++ Component1/Form1.cs
@@ -12,9 +12,17 @@ namespace Component1
 {
     public partial class Form1 : Form
     {
+        private CommandParser commandParser;
+
+        public string CurrentCommand {  get; set; }
+
+        public int x;
+        public int y;
+
+        
         public Form1()
         {
-            InitializeComponent();
+           
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -26,14 +34,30 @@ namespace Component1
 
             var g = Graphics.FromImage(pictureBox1.Image);
 
-            if ("circle".Equals(textBox1.Text))
-            {
-                g.FillEllipse(Brushes.Aquamarine, 10, 10, 100, 100);
-            }
-            else if ("square".Equals(textBox1.Text))
-            {
-                g.FillRectangle(Brushes.Aquamarine, 10, 10, 150, 150);
-            }
+
+            
+        }
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void DrawCursor(Graphics g, int X, int Y)
+        {
+            g.DrawEllipse(Pens.Red, X-5, Y-5, 10, 10);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        public void ClearPictureBox()
+        {
+           
+
         }
     }
 }
