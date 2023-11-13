5a57b7a29b7ea45f30210d47412d45c763a4cf9c
Deany256 <gdeandude@gmail.com>
2023-11-13 03:18:14 +0000

    final button 1 click

--- Component1/Form1.cs
+++ Component1/Form1.cs
@@ -14,15 +14,25 @@ namespace Component1
     {
         private CommandParser commandParser;
 
-        public string CurrentCommand {  get; set; }
+        public string CurrentCommand { get; set; }
 
         public int x;
         public int y;
 
-        
+
         public Form1()
         {
-           
+            InitializeComponent();
+            commandParser = new CommandParser(this);
+            label1.Text = "Confirmation of Actions or Errors will appear here";
+
+            if (pictureBox1.Image == null)
+            {
+                pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            }
+
+            // Subscribe to the Paint event
+            this.Paint += Form1_Paint;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -35,7 +45,38 @@ namespace Component1
             var g = Graphics.FromImage(pictureBox1.Image);
 
 
-            
+            if (textBox2.Text == "" && textBox1.Text == "")
+            {
+                commandParser.SendMessage("No commands detected.");
+            }
+            else if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                string[] commandArray = textBox1.Text.Split(' ');
+                if (commandArray[0] == "save")
+                {
+                    commandParser.ExecuteCommand(textBox1.Text);
+                }
+            }
+            else if (textBox1.Text != "")
+            {
+                string command = textBox1.Text;
+                commandParser.ExecuteCommand(command);
+
+                // Clear the TextBox
+                textBox1.Clear();
+            }
+            else if (textBox2.Text != "")
+            {
+                string[] lines = textBox2.Lines;
+
+                foreach (string line in lines)
+                {
+                    commandParser.ExecuteCommand(line);
+                }
+
+                // Clear the TextBox
+                textBox2.Clear();
+            }
         }
 
 
@@ -46,17 +87,17 @@ namespace Component1
 
         private void DrawCursor(Graphics g, int X, int Y)
         {
-            g.DrawEllipse(Pens.Red, X-5, Y-5, 10, 10);
+            g.DrawEllipse(Pens.Red, X - 5, Y - 5, 10, 10);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-           
+
         }
 
         public void ClearPictureBox()
         {
-           
+
 
         }
     }
